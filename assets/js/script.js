c99c50cb9e6021d99bdf8ae541b2a481589ebf0f
Vihan Perera <vihanpereraux@gmail.com>
2024-06-16 19:38:44 +0100

    add spline scene

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+import baffle from 'baffle';
+import { Application } from '@splinetool/runtime';
+
 // music animation
 const bars = document.getElementsByClassName('volume_bar');
 const volumeBarWrapper = document.getElementById('volume_bar_wrapper');
@@ -15,7 +18,7 @@ volumeBarWrapper.addEventListener('click', () => {
     if (isMuteButtonClicked) {
         backgroundScore.play();
         backgroundScore.volume = .35;
-        
+
         mutedLabel.innerText = '[UM] The Chicago Night';
         isMuteButtonClicked = false;
 
@@ -34,12 +37,33 @@ volumeBarWrapper.addEventListener('click', () => {
         clearInterval(barAnimationInterval);
         isMuteButtonClicked = true;
         mutedLabel.innerText = '[M] The Chicago Night';
-        
+
         for (let index = 0; index < bars.length; index++) {
             bars[index].style.height = 5 + 'px';
         }
     }
 });
+
+
+// revealing effect
+const scrollText = document.getElementById('scroll_text');
+// animations
+let text = baffle(scrollText, {
+    characters: 'scroll_down',
+    speed: 200
+}).start();
+setInterval(() => {
+    text.start();
+}, 2600);
+setInterval(() => {
+    text.reveal();
+}, 4000);
+
+
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/hDCubbyljWlwEQFh/scene.splinecode');
 
 
 
